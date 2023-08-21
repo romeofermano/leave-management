d2b0871f44d25ec56743d8eb518718b4d83bf9f7
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 15:13:27 +0800

    Added employee repository

--- src/main/java/com/synacy/leavemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/synacy/leavemanagement/repository/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.synacy.leavemanagement.repository;
 
+import com.synacy.leavemanagement.enums.EmployeeStatus;
 import com.synacy.leavemanagement.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    public Page<Employee> findAllByEmployeeStatus(EmployeeStatus employeeStatus, Pageable pageable);
 }
