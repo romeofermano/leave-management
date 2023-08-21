71b6dcbd6ba5ec4416771a6a365cd553b5807860
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 15:13:54 +0800

    Implemented fetch employees

--- src/main/java/com/synacy/leavemanagement/services/EmployeeService.java
+++ src/main/java/com/synacy/leavemanagement/services/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.synacy.leavemanagement.services;
 
+import com.synacy.leavemanagement.enums.EmployeeStatus;
 import com.synacy.leavemanagement.model.Employee;
 import com.synacy.leavemanagement.repository.EmployeeRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
@@ -12,6 +15,9 @@ public class EmployeeService {
     }
 
     public Page<Employee> fetchEmployees(int max, int page) {
+        int offset = page - 1;
+        Pageable pageable = PageRequest.of(offset, max);
 
+        return employeeRepository.findAll(pageable);
     }
 }
