fa8418a7076a5d97f332c0b029f86975062c2f89
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 22:09:39 +0800

    Added api error response

--- src/main/java/com/synacy/leavemanagement/web/exceptions/ApiErrorResponse.java
+++ src/main/java/com/synacy/leavemanagement/web/exceptions/ApiErrorResponse.java
@@ -1,4 +1,14 @@
 package com.synacy.leavemanagement.web.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class ApiErrorResponse {
+    private final String errorCode;
+    private final String errorMessage;
+
+    public ApiErrorResponse(String errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
 }
