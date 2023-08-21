d7f1438d582871e65c73363008601190db75ecea
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 15:30:37 +0800

    Added new method for getting employees

--- src/main/java/com/synacy/leavemanagement/employee/model/repository/EmployeeRepository.java
+++ src/main/java/com/synacy/leavemanagement/employee/model/repository/EmployeeRepository.java
@@ -2,10 +2,15 @@ package com.synacy.leavemanagement.employee.model.repository;
 
 import com.synacy.leavemanagement.enums.EmployeeStatus;
 import com.synacy.leavemanagement.employee.model.Employee;
+import com.synacy.leavemanagement.enums.RoleType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    public Page<Employee> findAllByEmployeeStatus(EmployeeStatus employeeStatus, Pageable pageable);
+    Page<Employee> findAllByEmployeeStatus(EmployeeStatus employeeStatus, Pageable pageable);
+    Page<Employee> findAllByEmployeeStatusAndRoleTypeIn(EmployeeStatus employeeStatus, List<RoleType> roleTypes,
+                                                        Pageable pageable);
 }
