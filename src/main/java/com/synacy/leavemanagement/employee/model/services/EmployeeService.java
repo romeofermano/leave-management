80ccf87be59df30792d9f9bc641b2c57177304ea
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 15:58:20 +0800

    Updated implementation

--- src/main/java/com/synacy/leavemanagement/employee/model/services/EmployeeService.java
+++ src/main/java/com/synacy/leavemanagement/employee/model/services/EmployeeService.java
@@ -3,9 +3,12 @@ package com.synacy.leavemanagement.employee.model.services;
 import com.synacy.leavemanagement.enums.EmployeeStatus;
 import com.synacy.leavemanagement.employee.model.Employee;
 import com.synacy.leavemanagement.employee.model.repository.EmployeeRepository;
+import com.synacy.leavemanagement.enums.RoleType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
 
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
@@ -18,6 +21,12 @@ public class EmployeeService {
         int offset = page - 1;
         Pageable pageable = PageRequest.of(offset, max);
 
-        return employeeRepository.findAllByEmployeeStatus(EmployeeStatus.ACTIVE, pageable);
+        return employeeRepository.findAllByEmployeeStatusAndRoleTypeIn(EmployeeStatus.ACTIVE,
+                Arrays.asList(RoleType.MANAGER, RoleType.MEMBER), pageable);
     }
+
+    // TODO: List all employees with the employee status is active
+    // TODO: Create new employees
+    // TODO: Update existing employees
+    // TODO: Terminate employees
 }
