243890928ddd7de32943124a5e079120edeba6d8
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-21 15:14:23 +0800

    Updated unit test for fetch employees

--- src/test/groovy/com/synacy/leavemanagement/services/EmployeeServiceSpock.groovy
+++ src/test/groovy/com/synacy/leavemanagement/services/EmployeeServiceSpock.groovy
@@ -1,5 +1,7 @@
 package com.synacy.leavemanagement.services
 
+import com.synacy.leavemanagement.enums.EmployeeStatus
+import com.synacy.leavemanagement.enums.RoleType
 import com.synacy.leavemanagement.model.Employee
 import com.synacy.leavemanagement.repository.EmployeeRepository
 import org.springframework.data.domain.Page
@@ -21,13 +23,19 @@ class EmployeeServiceSpock extends Specification {
         int max = 3
         int page = 1
         int expectedCount = 4
-        Page<Employee> employees = new PageImpl<>([Mock(Employee), Mock(Employee), Mock(Employee), Mock(Employee)])
+        Long managerId = 1L
+        RoleType roleTypeManager = RoleType.MANAGER
+        RoleType roleTypeMember = RoleType.MEMBER
+        Page<Employee> employees = new PageImpl<>([new Employee("Employee 1", roleTypeManager, 25),
+                                                   new Employee("Employee 2", roleTypeMember, 20, Mock(Employee)),
+                                                   new Employee("Employee 3", roleTypeManager, 25),
+                                                   new Employee("Employee 2", roleTypeMember, 20, Mock(Employee))])
 
         when:
         Page<Employee> result = employeeService.fetchEmployees(max, page)
 
         then:
-        1 * employeeRepository.findAll(_) >> employees
+        1 * employeeRepository.findAllByEmployeeStatus(EmployeeStatus.ACTIVE, _) >> employees
         employees == result
         result.getSize() == expectedCount
     }
