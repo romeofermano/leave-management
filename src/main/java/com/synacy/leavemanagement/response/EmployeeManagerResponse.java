79104ba60c14953594af153e9bd031370b1549d9
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-22 18:06:01 +0800

    Changed to employee manager

--- src/main/java/com/synacy/leavemanagement/response/EmployeeManagerResponse.java
+++ src/main/java/com/synacy/leavemanagement/response/EmployeeManagerResponse.java
@@ -13,7 +13,6 @@ public class EmployeeResponse {
     private final Integer totalLeaves;
     private final Integer currentLeaves;
     private final EmployeeStatus employeeStatus;
-    private final String manager;
 
     public EmployeeResponse(Employee employee) {
         this.id = employee.getId();
@@ -22,6 +21,5 @@ public class EmployeeResponse {
         this.totalLeaves = employee.getTotalLeaves();
         this.currentLeaves = employee.getCurrentLeaves();
         this.employeeStatus = employee.getEmployeeStatus();
-        this.manager = employee.getManager().getName();
     }
 }
