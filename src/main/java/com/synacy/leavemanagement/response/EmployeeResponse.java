0405745c93a4de92c9f0692d9593917c114b87b9
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-22 15:18:46 +0800

    Added new endpoint in getting all employee

---
+++ src/main/java/com/synacy/leavemanagement/response/EmployeeResponse.java
@@ -0,0 +1,8 @@
+package com.synacy.leavemanagement.responses;
+
+public class EmployeeResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String
+}
---
+++ src/main/java/com/synacy/leavemanagement/response/PageResponse.java
@@ -0,0 +1,2 @@
+package com.synacy.leavemanagement.response;public class PageResponse {
+}
