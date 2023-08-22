fa18d8a740bdd434afd9f7875bdd273757249fa2
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-22 15:21:04 +0800

    Added Page Response

--- src/main/java/com/synacy/leavemanagement/response/EmployeeResponse.java
+++ src/main/java/com/synacy/leavemanagement/response/EmployeeResponse.java
@@ -1,8 +1,27 @@
-package com.synacy.leavemanagement.responses;
+package com.synacy.leavemanagement.response;
 
+import com.synacy.leavemanagement.enums.EmployeeStatus;
+import com.synacy.leavemanagement.enums.RoleType;
+import com.synacy.leavemanagement.model.Employee;
+import lombok.Getter;
+
+@Getter
 public class EmployeeResponse {
-    private Long id;
-    private String name;
-    private String description;
-    private String
+    private final Long id;
+    private final String name;
+    private final RoleType roleType;
+    private final Integer totalLeaves;
+    private final Integer currentLeaves;
+    private final EmployeeStatus employeeStatus;
+    private final String manager;
+
+    public EmployeeResponse(Employee employee) {
+        this.id = employee.getId();
+        this.name = employee.getName();
+        this.roleType = employee.getRoleType();
+        this.totalLeaves = employee.getTotalLeaves();
+        this.currentLeaves = employee.getCurrentLeaves();
+        this.employeeStatus = employee.getEmployeeStatus();
+        this.manager = employee.getManager().getName();
+    }
 }
