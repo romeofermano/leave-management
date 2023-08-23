0ebfb8cd03999aa3a28ed31de2edeaaa920e1bfb
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-23 12:51:26 +0800

    Added employee member request

---
+++ src/test/groovy/com/synacy/leavemanagement/controller/EmployeeControllerSpec.groovy
@@ -0,0 +1,17 @@
+package com.synacy.leavemanagement.controller
+
+import spock.lang.Specification
+
+class EmployeeControllerSpec extends Specification {
+    void setup() {
+    }
+
+    def "GetEmployees"() {
+    }
+
+    def "GetListEmployees"() {
+    }
+
+    def "CreateEmployee"() {
+    }
+}
