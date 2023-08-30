a1e1982de6563e8d7be4db1a2a46c5984a8e6bdf
Carlina-Amaba <carlina.amaba@synacy.com>
2023-08-30 20:47:30 +0800

    fixed get employee leaves under manager

--- src/main/java/com/synacy/leavemanagement/leave/LeaveRepository.java
+++ src/main/java/com/synacy/leavemanagement/leave/LeaveRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,6 +18,12 @@ public interface LeaveRepository extends JpaRepository<Leave, Long> {
     Page<Leave> findAllExceptHRAdmin(RoleType roleType, Pageable pageable);
 
     Page<Leave> findAllByEmployee_Id(Long id, Pageable pageable);
+
+    @Query("SELECT l FROM Leave l JOIN l.employee e " +
+            "WHERE e.manager.id = :managerId " +
+            "AND e.id <> :managerId " +
+            "AND l.leaveStatus = 'PENDING'")
+    Page<Leave> findPendingLeavesByManagerIdExcludingManagerLeaves(@Param("managerId") Long managerId, Pageable pageable);
 
     Page<Leave> findAllByEmployeeManager_IdAndLeaveStatus(Long id, LeaveStatus leaveStatus, Pageable pageable);
 
